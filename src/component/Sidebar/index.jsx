8324ead174cc5e8a165de93851ecd3c0c68aeb56
Samben jerald <62704387+Samben01@users.noreply.github.com>
2023-10-07 12:19:40 +0530

    Add files via upload

---
+++ src/component/Sidebar/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { BiSolidDashboard } from 'react-icons/bi';
+import { CgProfile } from 'react-icons/cg';
+import { AiOutlineCustomerService } from 'react-icons/ai';
+
+const Sidebar = () => {
+  return (
+    <div class="sidenav">
+      <NavLink to={'/about'} className={'d-flex align-items-center gap-2'}>
+        <CgProfile />
+        <span>About</span>
+      </NavLink>
+      <NavLink to={'/dashboard'} className={'d-flex align-items-center gap-2'}>
+        <BiSolidDashboard />
+        <span>Dashboard</span>
+      </NavLink>
+      <NavLink to={'/service'} className={'d-flex align-items-center gap-2'}>
+        <AiOutlineCustomerService />
+        <span>Service</span>
+      </NavLink>
+    </div>
+  );
+};
+
+export default Sidebar;
